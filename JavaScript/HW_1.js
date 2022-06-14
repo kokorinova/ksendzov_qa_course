31d94da55fec39a6e0fb42fc94ae8c1e6553f8e6
kokorinova <valyakokorinov@gmail.com>
2022-06-14 20:15:52 +0300

    debug

--- JavaScript/HW_1.js
+++ JavaScript/HW_1.js
@@ -33,13 +33,13 @@ item_5 = item_3;
 //17. Создать переменную item_6.
 let item_6;
 //18. Создать переменную item_6_type
-let item_6_type
+let item_6_type;
 //19. Присвоить переменной item_6 значение 15
 item_6 = 15;
 //20. Присвоить переменной item_6_type тип переменной item_6
-item_6_type = typeof item_6
+item_6_type = typeof item_6;
 //21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
-console.log('item_6 == ' + typeof item_6 + ', item_6_type == ' typeof item_6_type);
+console.log('item_6 == ' + item_6 + ', item_6_type == ' + item_6_type);
 //22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7 = String(item_6);
 //23. Создать переменную item_7_type
@@ -47,7 +47,7 @@ let item_7_type;
 //24. Присвоить переменной item_7_type тип переменной item_7
 item_7_type = typeof(item_7);
 //25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
-console.log('item_7 == ' + typeof item_7 + ', item_7_type == ' + typeof item_7_type);
+console.log('item_7 == ' + item_7 + ', item_7_type == ' + item_7_type);
 //26. Создать переменную “age_1” и присвоить ей значение 10
 let age_1 = 10;
 //27. Создать переменную “age_2” и присвоить ей значение 18
@@ -60,9 +60,9 @@ let age_3 = 60;
 32. Если age_1 > age_3, вывести в консоль “Keep calm and look Culture channel”.
 33. Иначе выводите “Technical work”.*/
 if (age_1 < age_2) {
-    console.log("You don’t have access cause your age is " + age_1 + " It’s less then")
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then");
 } else if (age_1 >= age_2 && age_1 < age_3) {
-    console.log("Welcome!")
+    console.log("Welcome!");
 } else if (age_1 > age_3) {
     console.log("Keep calm and look Culture channel");
 } else console.log("Technical work");
